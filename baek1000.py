70cfaf3f53ea2248ba97066358172551cb5458bb
matchaing <69198709+matchaing@users.noreply.github.com>
2022-01-19 21:57:40 +0900

    baekjoon 1000
    baekjoon 1000

---
+++ baek1000.py
@@ -0,0 +1,10 @@
+a,b = input().split() #input으로 받고, split으로 각각 저장
+print(int(a)+int(b))
+
+
+# values = input()
+# values = values.split()
+# a = int(values[0])
+# b = int(values[1])
+
+# print(a+b)
