3147f127ad84f234681b627dab43e46ca6bd23e7
min <69198709+matchaing@users.noreply.github.com>
2023-05-15 16:07:03 +0900

    함수 수정

--- node.js/baek15964.js
+++ node.js/baek15964.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './예제.txt';
 const input = fs.readFileSync(filePath).toString().split(' ');
-
 let a = parseInt(input[0]);
 let b = parseInt(input[1]);
+function operator(a,b){
+return (a+b)*(a-b);
+}
 
-console.log((a+b)*(a-b));
+
+console.log(operator(a,b));
